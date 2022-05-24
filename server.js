5678af97d99d8926c0ea15e9910524fbbaf90a12
nbogie <neillbogie@googlemail.com>
2022-05-24 18:22:00 +0100

    finished rough draft

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+process.env.GOOGLE_APPLICATION_CREDENTIALS =
+  "secret/cohort4-auth-demo-firebase-adminsdk-7z5zx-641aa67888.json";
 const express = require("express");
-// const { initializeApp } = require("firebase-admin/app");
+const { initializeApp } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+const cors = require("cors");
+
+initializeApp(); //default credential
 
 const app = express();
-const port = process.env.PORT || 4000;
-const cors = require("cors");
 app.use(cors());
+const port = process.env.PORT || 4000;
+
 app.get("/", (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.send("The time is not secret!: " + new Date());
@@ -12,35 +18,46 @@ app.get("/", (req, res) => {
 
 app.get("/secret", (req, res) => {
   console.log("req for secret: ", { headers: req.headers });
-
-  try {
-    const token = req.get("Authorization");
-    console.log({ token });
-    // res.set('Access-Control-Allow-Origin', '*');
-    res.send("SECRET!!!!!!!!: " + randomSentence());
-  } catch (err) {
-    console.error(err);
-    res.send("error! an error happened!");
+  const authHeader = req.get("Authorization");
+  if (!authHeader || authHeader.length < 20) {
+    return res.status(401).send("no or bad auth header");
   }
+  const idToken = authHeader.slice(7); //get rid of "Bearer " prefix
+
+  getAuth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      const uid = decodedToken.uid;
+      console.log("VERIFIED TOKEN", uid, decodedToken);
+      res.send("SECRET!!!!!!!!: " + randomSentence());
+    })
+    .catch((error) => {
+      console.error("NOT A GOOD TOKEN - don't reveal secret");
+      res.send("I will not reveal the secrets to you!"); //in reality we'd send a 401 status?
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
 function randomSentence() {
   const articles = ["the"];
   const nouns =
-    "bus shark beatboxer love teacher poem thought cinnamon donut crepe bearclaw experiment science policy bear kangaroo shaman ninja robot pirate helicopter band riot song".split(
+    "bus shark falcon beatboxer love teacher poem thought cinnamon donut crepe bearclaw experiment science policy bear kangaroo shaman ninja robot pirate helicopter band riot song".split(
       " "
     );
   const verbs =
-    "ate hugged murdered composed conceived analysed cooked sang".split(" ");
-  const adjectives =
-    "hungry blue imaginary lovely cinnamon violent sweet god-fearing abyssal nightmarish rainbow 70s musical smelly flowery well-intentioned creative owlish otherworldly".split(
+    "ate hugged murdered composed conceived analysed cooked sang wrestled petted fed tutored neuralyzed".split(
       " "
     );
-  return [articles, nouns, verbs, articles, nouns].map(pick).join(" ");
+  const adjectives =
+    "hungry blue imaginary peanut-butter-covered lovely cinnamon violent sweet god-fearing sugar-coated weapon-grade apocalyptic abyssal nightmarish rainbow 70s musical smelly flowery well-intentioned creative owlish otherworldly".split(
+      " "
+    );
+  return [articles, adjectives, nouns, verbs, articles, nouns]
+    .map(pick)
+    .join(" ");
 }
 function pick(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
