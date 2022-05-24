f929ca71881aea833907005874216188306a8a08
nbogie <neillbogie@googlemail.com>
2022-05-24 23:04:46 +0100

    moved secrets to book of secrets

---
+++ bookOfSecrets.js
@@ -0,0 +1,23 @@
+function getSecretWisdom() {
+  const articles = ["the"];
+  const nouns =
+    "arrest-warrant band bear bearclaw beatboxer board-game boardgamer boyfriend bus call-centre cinnamon coroner cowboy crepe detective dog donut dungeon-master experiment falcon game girlfriend good-bye helicopter icon kangaroo love magician ninja ore particle-accelerator piano pirate pizza playwright poem policy potato reviewer rhyme riot robot rule-book science screenplay shaman shark song space-mobile submarine summoning teacher theory thought worker".split(
+      " "
+    );
+  const adjectives =
+    "70s abyssal apocalyptic blue burnt-out cinnamon creative dragon-borne drug-addicted eloquent enlightened flimsy flowery giant glowing god-fearing golden hallucinatory hungry imaginary immobile lovely miniature musical mythical nightmarish ninja obnoxious otherworldly owlish parallel-universe peanut-butter-covered pirate poorly-written radioactive rainbow rusty smelly stealthy sugar-coated sweet sweet tearful unconscious violent virtual weapon-grade well-intentioned".split(
+      " "
+    );
+  const verbs =
+    "analysed arrested ate befuddled betrayed chastised composed compromised conceived confused cooked coveted created fed fought harassed hugged invented killed kissed murdered nagged painted praised programmed robbed sang seduced serenaded sued suffocated summoned taught tutored treated wrestled".split(
+      " "
+    );
+  return [articles, adjectives, nouns, verbs, articles, nouns]
+    .map(pick)
+    .join(" ");
+}
+function pick(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+module.exports = { getSecretWisdom };
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ process.env.GOOGLE_APPLICATION_CREDENTIALS =
 const express = require("express");
 const { initializeApp } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
-const cors = require("cors");
+const { getSecretWisdom } = require("./bookOfSecrets");
 
+const cors = require("cors");
 initializeApp(); //default credential
 
 const app = express();
@@ -12,7 +13,6 @@ app.use(cors());
 const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
   res.send("The time is not secret!: " + new Date());
 });
 
@@ -29,7 +29,7 @@ app.get("/secret", (req, res) => {
     .then((decodedToken) => {
       const uid = decodedToken.uid;
       console.log("VERIFIED TOKEN", uid, decodedToken);
-      res.send("SECRET!!!!!!!!: " + randomSentence());
+      res.send("🤐: " + getSecretWisdom() + "🤫");
     })
     .catch((error) => {
       console.error("NOT A GOOD TOKEN - don't reveal secret");
@@ -40,25 +40,3 @@ app.get("/secret", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-function randomSentence() {
-  const articles = ["the"];
-  const nouns =
-    "bus shark falcon beatboxer love teacher poem thought cinnamon donut crepe bearclaw experiment science policy bear kangaroo shaman ninja robot pirate helicopter band riot song".split(
-      " "
-    );
-  const verbs =
-    "ate hugged murdered composed conceived analysed cooked sang wrestled petted fed tutored neuralyzed".split(
-      " "
-    );
-  const adjectives =
-    "hungry blue imaginary peanut-butter-covered lovely cinnamon violent sweet god-fearing sugar-coated weapon-grade apocalyptic abyssal nightmarish rainbow 70s musical smelly flowery well-intentioned creative owlish otherworldly".split(
-      " "
-    );
-  return [articles, adjectives, nouns, verbs, articles, nouns]
-    .map(pick)
-    .join(" ");
-}
-function pick(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
