6068e96daad221e540de6246c4eebc5f760aeee5
nbogie <neillbogie@googlemail.com>
2022-05-25 19:32:03 +0100

    docs

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+
 const port = process.env.PORT || 4000;
 
+//This route stays public for all
 app.get("/", (req, res) => {
   res.send("Time (not secret): " + new Date());
 });
 
+//TODO: Your task will be to secure this route to prevent access by those who are not, at least, logged in.
 app.get("/secret", (req, res) => {
+  //Eventual plan:
+  //1. authHeader = get the value of the Authorization header
+  //2. potentialToken = strip the "Bearer " prefix from authHeader
+  //3. if (potentialToken is verified legit)
+  //4.     return protected info in response
+  //5. else
+  //       say access denied in response
   res.send("🤐: " + getSecretWisdom() + "🤫");
 });
 
