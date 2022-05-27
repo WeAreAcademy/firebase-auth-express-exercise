2b1f0f30f1ca13602d539d14d1f7f80c5e72b831
nbogie <neillbogie@googlemail.com>
2022-05-28 00:49:41 +0100

    use middleware again

--- server.js
+++ server.js
@@ -13,35 +13,36 @@ app.use(cors());
 
 const port = process.env.PORT || 4000;
 
-//This route stays public for all
-app.get("/", (req, res) => {
-  res.send("Time (not secret): " + new Date());
-});
-
-//TODO: Your task will be to secure this route to prevent access by those who are not, at least, logged in.
-app.get("/wisdom", async (req, res) => {
+//(this is a middleware function)
+//if the request has a verified accessToken in Authorization header,
+//then it puts the user id in as req.uid and proceeds to the next handler
+//else it replies to the client immediately with a 401 and stops the flow.
+async function checkIsAuthenticated(req, res, next) {
   const authHeaderVal = req.header("Authorization");
-
-  if (!authHeaderVal || authHeaderVal.length < 20) {
-    res.status(401).send("No authorization header (or it is too short)");
-    console.log("ignoring request with no (or short) auth header");
+  if (!authHeaderVal) {
+    res.status(401).send("No authorization header.");
     return;
   }
   const idToken = authHeaderVal.slice(7);
   try {
     const decodedToken = await getAuth().verifyIdToken(idToken);
-    console.log("VERIFIED TOKEN.  uid: ", decodedToken.uid);
-    res.send("🤐: " + getAncientWisdom() + "🤫");
+    //put whatever you like into the request, for your route handlers to make use of.
+    req.uid = decodedToken.uid;
+    next();
   } catch (err) {
-    res.status(401).send("token did not verify");
+    res.status(401).send("Token did not verify.");
   }
-  //Eventual plan:
-  //1. authHeader = get the value of the Authorization header
-  //2. potentialToken = strip the "Bearer " prefix from authHeader
-  //3. if (potentialToken is verified legit)
-  //4.     return protected info in response
-  //5. else
-  //       say access denied in response
+}
+
+//This route stays public for all
+app.get("/", (req, res) => {
+  res.send("Time (not secret): " + new Date());
+});
+
+app.get("/wisdom", checkIsAuthenticated, async (req, res) => {
+  // @ts-ignore // type-checker doesn't think there should be a uid on the request object.
+  console.log("uid: ", req.uid);
+  res.send("🤐: " + getAncientWisdom() + "🤫");
 });
 
 app.listen(port, () => {
