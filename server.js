e2bf89065d173ac5f4c144fb4839cbf38fec6100
nbogie <neillbogie@googlemail.com>
2022-05-27 14:58:33 +0100

    done verify token - naive

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+//TODO: move out of js into environment
+process.env.GOOGLE_APPLICATION_CREDENTIALS =
+  "secrets/firebase-service-account-secrets.json";
+
 const express = require("express");
 const { getAncientWisdom } = require("./bookOfAncientWisdom");
-
 const cors = require("cors");
 
+const { initializeApp } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+
+const firebaseApp = initializeApp();
+
+console.log();
 const app = express();
 app.use(cors());
 
@@ -15,6 +24,24 @@ app.get("/", (req, res) => {
 
 //TODO: Your task will be to secure this route to prevent access by those who are not, at least, logged in.
 app.get("/wisdom", (req, res) => {
+  const authHeaderValue = req.get("Authorization") || "";
+
+  const [junk, accessToken] = authHeaderValue.split(" ");
+  if (!accessToken || accessToken.length < 10) {
+    console.log("ignoring user with bad or missing authorization header value");
+    res.status(401).send("no or bad auth header");
+    return;
+  }
+  getAuth()
+    .verifyIdToken(accessToken)
+    .then((decodedToken) => {
+      console.log("HURRAY! verified token.  ", decodedToken);
+      res.send("🤐: " + getAncientWisdom() + "🤫");
+    })
+    .catch((nope) => {
+      res.status(401).send("your token didn't check out! no wisdom for you.");
+    });
+
   //Eventual plan:
   //1. authHeader = get the value of the Authorization header
   //2. potentialToken = strip the "Bearer " prefix from authHeader
@@ -22,7 +49,6 @@ app.get("/wisdom", (req, res) => {
   //4.     return protected info in response
   //5. else
   //       say access denied in response
-  res.send("🤐: " + getAncientWisdom() + "🤫");
 });
 
 app.listen(port, () => {
